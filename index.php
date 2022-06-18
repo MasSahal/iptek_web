db3fba9ffbba68aca5dadb824fa19dec9bd82484
Mas Sahal <52941780+MasSahal@users.noreply.github.com>
2022-06-18 08:14:25 +0700

    add first

---
+++ index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.css">
+
+    <title>Disnaker Kota Cirebon</title>
+
+</head>
+
+<body class="">
+    <h1>Hello World!</h1>
+
+    <script src="js/jquery.js"></script>
+    <script src="js/popper.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+
+</html>
